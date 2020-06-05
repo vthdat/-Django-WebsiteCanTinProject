8b1846a0976b59d1f3e2b5cbc5935941273023b8
Dat Vu <vd.thanhdat@gmail.com>
2020-06-05 15:02:34 +0700

    feat: User creation and login

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    full_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    point = models.FloatField(default = 0)
