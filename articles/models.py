99c343a3558bdcb757b1f29f6e2be312be372f75
Dat Vu <vd.thanhdat@gmail.com>
2020-06-11 14:44:52 +0700

    feat: add Article model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=300)
+    date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='static/images/articles')
+
+    def __str__(self):
+        return self.title
+        
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[str(self.id)])
