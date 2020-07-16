d40161b149f180e7ad56727d6cfcb4eb68a4d30b
Dat Vu <vd.thanhdat@gmail.com>
2020-07-16 13:46:54 +0700

    feat: custom food list

--- Food/views.py
+++ Food/views.py
@@ -9,8 +9,14 @@ from cart.cart import Cart
 class FoodListView(ListView):
     model = Food
     template_name = 'food_list.html'
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['beverage_list'] = Food.objects.filter(food_type='BE')
+        context['breakfast_list'] = Food.objects.filter(food_type='BR')
+        context['lunch_list'] = Food.objects.filter(food_type='LU')
+        context['dinner_list'] = Food.objects.filter(food_type='DI')
+        context['snack_list'] = Food.objects.filter(food_type='SN')
         context['cart_product_form'] = CartAddFoodForm()
         return context
 
@@ -20,16 +26,9 @@ class FoodDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['food_list'] = Food.objects.filter(food_type=self.object.food_type).exclude(id=self.object.id)
         context['cart_product_form'] = CartAddFoodForm()
         return context
-
-def food_detail(request, id):
-    food = get_object_or_404(Food, id=id, available=True)
-    cart_product_form = CartAddFoodForm()
-    return render(request, 'food_detail.html', {'food': food,
-                                                'cart_product_form': cart_product_form})
-
-    
 
 class FoodBreakfastListView(ListView):
     model = Food
--- pages/views.py
+++ pages/views.py
@@ -7,11 +7,13 @@ from cart.cart import Cart
 # Create your views here.
 
 class HomePageView(TemplateView):
+    model = Food
     template_name = 'new_home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['cart_product_form'] = CartAddFoodForm()
-        context['food_list'] = Food.objects.all()
+        context['food_list'] = Food.objects.exclude(food_type='BE')[:6]
+        context['beverage_list'] = Food.objects.filter(food_type='BE')[:6]
         return context
     
